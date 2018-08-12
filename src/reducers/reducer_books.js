d5305e6acc1c263a40629e2f1544035bf1f28b96
Pallavi Nawani <37789944+Pall104@users.noreply.github.com>
2018-08-12 16:32:12 +0530

    book-list using redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function Books() {
+    return [
+        {'title': 'Javascript:Good Parts','pages':100},
+        {'title':'Harry Potter','pages':200},
+        {'title':'The Dark Tower','pages':250},
+        {'title':'Eloquent Ruby','pages':150}
+    ]
+}
